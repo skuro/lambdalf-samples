a5e206afc1670e7fb8fa08b29119dd2aa4ebd1c7
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-05-03 09:17:20 +0200

    - Bootstrap clj code with a spring bean - Bootstrap data to include clj webscripts - Trying to customize the explorer UI

--- alfrescobox/src/main/clojure/alfrescobox.clj
+++ alfrescobox/src/main/clojure/alfrescobox.clj
@@ -1,2 +1,30 @@
 (ns alfrescobox
-  (:require [alfresco.behave :as b]))
+  (:require [alfresco.behave :as b]
+  			[alfresco.model :as m]
+  			[alfresco.nodes :as n]))
+
+(def *ticket-size* (atom 25 :validator number?))
+
+;; as seen here: http://blog.raphinou.com/2009/03/generate-random-string-in-clojure.html
+(def VALID-CHARS
+     (map char (concat (range 48 58) ; 0-9
+                       (range 65 91) ; A-Z
+                       (range 97 123))))
+
+(defn rand-char []
+  (rand-nth VALID-CHARS))
+
+(defn rand-str [length]
+  (apply str (take length (repeatedly rand-char))))
+
+(defn gen-ticket
+  []
+  (rand-str @*ticket-size*))
+
+(defn downloadable-aspect
+  [node qname]
+  (n/set-properties! node "abx:ticket" (gen-ticket)))
+
+(defn init
+  []
+  (b/on-add-aspect! (m/qname "abx:downloadable") downloadable-aspect))
