4455c0ffebf868c0717367cf9426d3d2e88cb7e5
Carlo Sciolla <carlo.sciolla@gmail.com>
2011-09-23 01:51:07 +0200

    better docstrings, externalize action implementation

--- alfrescobox/src/main/clojure/alfrescobox.clj
+++ alfrescobox/src/main/clojure/alfrescobox.clj
@@ -13,34 +13,49 @@
                        (range 65 91) ; A-Z
                        (range 97 123))))
 
-(defn rand-char []
+(defn rand-char
+  "Picks a random character from the available sets"
+  []
   (rand-nth VALID-CHARS))
 
-(defn rand-str [length]
+(defn rand-str
+  "Generates a random string of the given length"
+  [length]
   (apply str (take length (repeatedly rand-char))))
 
-(defn gen-ticket []
+(defn gen-ticket
+  "Generates a downloadable ticket"
+  []
   (rand-str @*ticket-size*))
 
-(defn downloadable-aspect [node qname]
+(defn downloadable-aspect
+  "Applies the abx:downloadable aspect including a randomly generated ticket property"
+  [node qname]
   (n/set-properties! node "abx:ticket" (gen-ticket)))
 
+;; Generates bootstrap/initialization code
 (gen-class :name  alfrescobox.Startup
            :prefix "abx-"
            :methods [[boot [] void]])
 
 (defn abx-boot [this]
   (b/on-add-aspect! (m/qname "abx:downloadable") downloadable-aspect))
+;;
 
+(defn- email-action-impl [_ action node]
+  (println "Recipients are:")
+  (println (str "xxxx" (bean action)))
+  (map println (seq (.getParameterValue action "to"))))
+
+;; Define a new Action class that can be used in Spring initialization
 (defrecord EmailTicketAction []
-  Action
-  (needs-params [this]
-                [(act/param {:name "to"
-                             :type "d:text"
-                             :mandatory true
-                             :multi true
-                             :label "email-ticket"})])
-  
-  (exec [this action node]
-        (println "Recipients are:")
-        (map println (seq (.getParameterValue action "to")))))
+    Action
+    (needs-params [this]
+      [(act/param {:name "to"
+                   :type "d:text"
+                   :mandatory true
+                   :multi true
+                   :label "email-ticket"})])
+    
+    (exec [this action node]
+      (email-action-impl this action node)))
